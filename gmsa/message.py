160446e42fad40e676ca91e7b03058ab885d8b90
Matt Black <github@mafro.net>
2024-03-13 14:51:52 +1100

    Rename project to gmsa and use pypa/hatch

--- gmsa/message.py
+++ gmsa/message.py
@@ -10,9 +10,9 @@ from typing import List, Optional, Union
 from httplib2 import Http
 from googleapiclient.errors import HttpError
 
-from simplegmail import label
-from simplegmail.attachment import Attachment
-from simplegmail.label import Label
+from gmsa import label
+from gmsa.attachment import Attachment
+from gmsa.label import Label
 
 
 class Message(object):
